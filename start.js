bf982a5a6edd830d4f9765c4e9a332351ca40777
Viacheslav Kushnir <viacheslav.kushnir@playtika.com>
2022-05-02 03:08:45 +0300

    Move to es6 + error request

--- start.js
+++ start.js
@@ -1,30 +1,32 @@
-import httpServer from 'http-server'
-
-const fs = require('fs')
-const util = require('util')
-
-const readFile = util.promisify(fs.readFile)
+import { createServer } from 'http-server'
+import { readFile as readFileFS } from 'fs'
+import { promisify } from 'util'
 
 const PORT = 8070
 
-const server = httpServer.createServer({
+const readFile = promisify(readFileFS)
+
+const delay = time => new Promise(resolve => setTimeout(resolve, time))
+
+const server = createServer({
   root: '/backend',
   // ext: 'json',
   before: [async (req, res) => {
     const filePath = `./backend/${req.url}.json`
-    console.log(' - req.url FILE: >', req.method, `${req.url}.json`); // eslint-disable-line no-console
-    // options.logFn(req, res);
-    // res.emit('next');
+    console.log(' - req.url FILE: >', req.method, `${req.url}.json`) // eslint-disable-line no-console
+    // options.logFn(req, res)
+    // res.emit('next')
     try {
-      const file = await readFile(filePath);
-      res.write(file);
+      await delay(500)
+      const file = await readFile(filePath)
+      res.write(file)
     } catch (e) {
-      res.write(`ERROR: ${e}`);
+      res.write(`ERROR: ${e}`)
     }
-    res.end();
+    res.end()
   }],
 })
 
 server.listen(PORT, () => {
-  console.log(' - START:20 >', PORT); // eslint-disable-line no-console
+  console.log(' - START:20 >', PORT) // eslint-disable-line no-console
 })
