24bd00d3d764c6e6c5cff5a92b3f7aa04c91fb62
Slaawwa <slaawwa@gmail.com>
2022-05-15 15:53:10 +0300

    Add topics + rules + errors + logs api call

--- start.js
+++ start.js
@@ -12,13 +12,23 @@ const server = createServer({
   root: '/backend',
   // ext: 'json',
   before: [async (req, res) => {
-    const filePath = `./backend/${req.url}.json`
+    let filePath = `./backend/${req.url}.json`
+    console.log(res.method);
+    if (res.method === 'DELETE' && /\/topic\//.test(req.url)) {
+      filePath = `./backend/api/topicDeleteID.json`
+    }
     console.log(' - req.url FILE: >', req.method, `${req.url}.json`) // eslint-disable-line no-console
     // options.logFn(req, res)
     // res.emit('next')
     try {
       await delay(500)
       const file = await readFile(filePath)
+      res.setHeader('Content-Type', 'application/json');
+      if (/Error/.test(req.url)) {
+        // res.writeHead(1001)
+        const jsonContent = JSON.parse(file)
+        res.writeHead(400, `${jsonContent.status} ${jsonContent.message}`);
+      }
       res.write(file)
     } catch (e) {
       res.write(`ERROR: ${e}`)
